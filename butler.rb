59e52de225d6107bfe3a3c0f1e37ceea2818bb5f
Austen Allred <austen.allred@gmail.com>
2014-01-06 07:50:11 -0700

    return values

--- butler.rb
+++ butler.rb
@@ -1,15 +1,7 @@
 class Butler
 
-  def open_front_door
-    puts "Opening the front door"
+  def open_door(location)
+    puts "Opening the #{location} door"
   end  
 
-  def open_back_door
-    puts "Opening the back door"
-  end
-
 end
-
-jeeves = Butler.new
-
-jeeves.open_front_door
--- personal_chef.rb
+++ personal_chef.rb
@@ -1,11 +1,18 @@
 class PersonalChef
 
-  def make_toast(color = 'golden', quality = 'great')
-    puts "Making your #{color} color, #{quality} quality toast!"
+  def make_toast(color)
+    puts "Making your #{color} toast!"
+    return self
+  end
+
+  def make_eggs(quantity)
+    puts "Making you #{quantity} eggs!"
+    return self
   end
 
   def make_milkshake
     puts "Bringing the boys to the yard!"
+    return self
   end
 
 end
