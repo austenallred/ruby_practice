7fede0aecdc56c2e7e9fb1660d5783b9ee9e6900
Austen Allred <austen.allred@gmail.com>
2014-01-07 06:49:34 -0700

    added conditionals

--- personal_chef.rb
+++ personal_chef.rb
@@ -54,4 +54,15 @@ require 'date'
       puts "This is the #{thing} thing and this is the #{number} number"
     end
   end
+
+  def water_status(minutes)
+    if minutes < 7
+      puts "The water is not boiling yet."
+    elsif minutes == 7
+      puts "It's just barely boiling."
+    elsif minutes == 8
+      puts "Hot! Hot! Hot!"
+    end
+    return self
+  end
 end
